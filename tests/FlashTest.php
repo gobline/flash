3cc9dd7c548d2fdca19b54dfbcf2153fb7a5c44b
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-12-03 21:58:41 +0100

    fix tests

--- tests/FlashTest.php
+++ tests/FlashTest.php
@@ -24,6 +24,7 @@ class FlashTest extends PHPUnit_Framework_TestCase
         @session_start();
 
         $this->flash = new Flash();
+        $this->flash->initialize();
     }
 
     public function testFlashNext()
